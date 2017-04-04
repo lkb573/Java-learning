91b9c18c4f96676e14b65a6ad60676d07e768f95
gooner573 <lkb573@gmail.com>
2017-04-04 14:42:41 +0900

    test commit_3

---
+++ src/PiboNachi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class PiboNachi {
+    public static void main(String[] args) {
+        int piboNum;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("피보나치 수 :");
+        piboNum = scan.nextInt();
+
+        int beforeNum = 0, nextNum = 1, temp = 0;
+
+        for (int i = 1; i < piboNum; i++) {
+            temp = beforeNum + nextNum;
+            beforeNum = nextNum;
+            nextNum = temp;
+        }
+
+        System.out.print("result : " + temp);
+    }
+}
